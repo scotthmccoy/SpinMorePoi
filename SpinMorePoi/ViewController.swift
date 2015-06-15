568264caee1426446842277666dcbea9f9f95e88
Scott McCoy <scott.mccoy@openx.com>
2015-06-15 16:16:24 -0700

    Initial Commit

---
+++ SpinMorePoi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpinMorePoi
+//
+//  Created by Scott McCoy on 6/15/15.
+//  Copyright (c) 2015 ScottSoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
