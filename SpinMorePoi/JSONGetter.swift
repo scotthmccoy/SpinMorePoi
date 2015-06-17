907f61f85b2222a436a28e52c94baa6b3c874080
Scott McCoy <scott.mccoy@openx.com>
2015-06-17 14:26:49 -0700

    Buffed up JSONGetter. Need to pass errors from private funcs to public class func

--- SpinMorePoi/JSONGetter.swift
+++ SpinMorePoi/JSONGetter.swift
@@ -8,23 +8,46 @@
 
 import Foundation
 
-func getJSON(urlToRequest: String) -> NSData? {
-    if let url = NSURL(string: urlToRequest){
+
+class JSONGetter {
+    
+    class func downloadAndParseJSON(urlString: String, successBlock:(Dictionary<String,AnyObject>)->(), failureBlock:(NSError?)->()) {
+        
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) {
+            if let jsonData = JSONGetter.getJSON(urlString) {
+                if let dict = JSONGetter.parseJSON(jsonData) {
+                    
+                    dispatch_async(dispatch_get_main_queue()) {
+                        successBlock(dict)
+                    }
+                }
+            }
+            
+            dispatch_async(dispatch_get_main_queue()) {
+                failureBlock(nil)
+            }
+        }
+    }
+
+    private class func getJSON(urlString: String) -> NSData? {
+        if let url = NSURL(string: urlString){
+            var error: NSError?
+            if let ret = NSData(contentsOfURL: url, options: NSDataReadingOptions.DataReadingUncached, error: &error) {
+                return ret
+            }
+            DebugLog("ERROR \(error)")
+        }
+        
+        return nil
+    }
+
+    private class func parseJSON(inputData: NSData) -> Dictionary<String,AnyObject>? {
         var error: NSError?
-        if let ret = NSData(contentsOfURL: url, options: NSDataReadingOptions.DataReadingUncached, error: &error) {
+        if let ret = NSJSONSerialization.JSONObjectWithData(inputData, options: NSJSONReadingOptions.MutableContainers, error: &error) as? Dictionary<String,AnyObject> {
             return ret
         }
         DebugLog("ERROR \(error)")
+        return nil
     }
-    
-    return nil
 }
 
-func parseJSON(inputData: NSData) -> Dictionary<String,AnyObject>? {
-    var error: NSError?
-    if let ret = NSJSONSerialization.JSONObjectWithData(inputData, options: NSJSONReadingOptions.MutableContainers, error: &error) as? Dictionary<String,AnyObject> {
-        return ret
-    }
-    DebugLog("ERROR \(error)")
-    return nil
-}
